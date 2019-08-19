6c444f4618777320ba08381dd544e30cc37723e7
Shota Izumiyama <shota@kagari>
2019-08-19 10:06:58 +0900

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   app/Main.hs
    	modified:   src/Lib.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 module Main where
 import Text.CSV
 import Data.Either
-import Statistics.Sample
+import Statistics.Sample as S
 import qualified Data.Vector as V
 import System.Environment
 
+import Lib
 
 main :: IO ()
 main = do
@@ -18,18 +19,7 @@ main = do
   putStrLn ("This is " ++ progname ++ "\n"
                 ++ "File: " ++ dataFileName' ++ "\n"
                 ++ "sample num: " ++ (show $ V.length vec) ++ "\n"
-                ++ "mean: " ++ (show $ mean vec) ++ "\n"
-                ++ "stdDev: " ++ (show $ stdDev vec) ++ ".\n")
+                ++ "mean: " ++ (show $ S.mean vec) ++ "\n"
+                ++ "stdDev: " ++ (show $ S.stdDev vec) ++ ".\n")
 
 dataFileName = "./data/test.csv"
-getDoubleRecord xs = foldr (\x acc -> if x == [""] then acc else (map read x :: [Double]):acc) [] xs
-overThreshold :: Ord a => a -> [[a]] -> Bool
-overThreshold th (x:y:xs) = (head $ tail y) >= th && (head $ tail x) < th 
-overThreshold th (x:[]) = False
-
-filterList :: ([a] -> Bool) -> [a] -> [a]
-filterList p [] = []
-filterList p (x:xs) = if p (x:xs) then x:(filterList p xs) else filterList p xs
-getDiffList :: Num a => [[a]] -> [a]
-getDiffList (x:[]) = []
-getDiffList (x:xs) = ((head $ head xs) - (head x)):(getDiffList xs)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
     ( someFunc
+    , toDoubleRecord
+    , toDiffList
+    , overThreshold
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+toDoubleRecord xs = foldr (\x acc -> if x == [""] then acc else (map read x :: [Double]):acc) [] xs
+overThreshold :: Ord a => a -> [[a]] -> Bool
+overThreshold th (x:y:xs) = (head $ tail y) >= th && (head $ tail x) < th 
+overThreshold th (x:[]) = False
+
+filterList :: ([a] -> Bool) -> [a] -> [a]
+filterList p [] = []
+filterList p (x:xs) = if p (x:xs) then x:(filterList p xs) else filterList p xs
+toDiffList :: Num a => [[a]] -> [a]
+toDiffList (x:[]) = []
+toDiffList (x:xs) = ((head $ head xs) - (head x)):(getDiffList xs)
+
+data Sample = Sample a a
+timeSample :: (Num a) => Sample a -> a
+timeSample (Sample x y) = x
+
