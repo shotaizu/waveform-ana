e4ae1b85caadac3306a9267b02f7c35eeeb6c73c
Shota Izumiyama <shota@izumiyama>
2019-08-30 19:29:56 +0900

    modifying

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 import Text.CSV
 import Data.Either
-import Statistics.Sample
-import Statistics.Function
 import qualified Data.Vector as V
+import Statistics.Function
+import Statistics.Sample
+import System.Directory
 import System.Environment
 import Data.Maybe
 
@@ -13,19 +14,42 @@ main :: IO ()
 main = do
   args <- getArgs
   progname  <- getProgName
-  let dataFileName' = if args == [] then dataFileName else head args
-  let threshold = if length args < 2 then 1.0 else read $ head $ tail args
-  d <- parseCSVFromFile dataFileName'
-  let ds = head $ rights $ d:[]
-      difflist = toDiffList $ catMaybes $ toXWithThresholdMaybe threshold $ (mapPoints read $ toPoints ds :: [Point Double])
-      vec = V.fromList difflist
-  putStrLn ("This is " ++ progname ++ "\n"
+  if length args < 1
+    then
+      putStrLn  (  progname ++ "\n"
+                ++ "Usage: \n"
+                ++ "\t$ " ++ progname ++ " {threshold} {data directory or file}\n"
+                )
+    else do
+      let
+        dataFileName' = if args == [] then dataFileName else head args where
+          dataFileName  = "./data/test.csv"
+        threshold = if length args < 2 then 1.0 else read $ head $ tail args
+      flagDoesFile <- doesFileExist dataFileName'
+      flagDoesDirectory <- doesDirectoryExist dataFileName'
+      let
+        dataFileNameType = if flagDoesFile then File else if flagDoesDirectory then Directory else FNothing
+      dataFiles <- case dataFileNameType of
+        File      -> return (dataFileName':[])
+        Directory -> fmap (\x -> fmap (\y -> (dataFileName' ++ "/" ++ y)) x) $ listDirectory dataFileName'
+        FNothing  -> return []
+      print dataFiles
+      d <- parseCSVFromFile dataFileName'
+      let
+        d' = map parseCSVFromFile dataFiles
+        ds = head $ rights $ d:[]
+        --ds' = map (\x -> do y <- x; return (rights y)) d'
+        difflist = toDiffList $ catMaybes $ toXWithThresholdMaybe threshold $ (mapPoints read $ toPoints ds :: [Point Double])
+        --difflist' = toDiffList $ catMaybes $ toXWithThresholdMaybe threshold $ (mapPoints read $ toPoints ds :: [Point Double])
+        vec = V.fromList difflist
+      putStrLn  (  "This is " ++ progname ++ "\n"
                 ++ "File: " ++ dataFileName' ++ "\n"
                 ++ "sample num: " ++ (show $ V.length vec) ++ "\n"
                 ++ "mean: " ++ (show $ mean vec) ++ "\n"
                 ++ "stdDev: " ++ (show $ stdDev vec) ++ ".\n"
                 ++ "min: " ++ (show $ fst $ minMax vec) ++ ".\n"
                 ++ "max: " ++ (show $ snd $ minMax vec) ++ ".\n"
-           )
+                )
+  
+data FilePathType = File | Directory | FNothing
 
-dataFileName  = "./data/test.csv"
